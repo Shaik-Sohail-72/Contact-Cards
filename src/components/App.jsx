4826252baaca94eba0b5f0b8a1e558e94e02f754
sohail <shaiksohailhu7n@gmail.com>
2022-05-27 21:08:59 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function Createcard(contacts)
+{
+  return <Card 
+  //id={contacts.id}
+  key={contacts.id}
+  name={contacts.name}
+    img={contacts.imgURL}
+    tel={contacts.phone}
+    email={contacts.email}
+  />
+}
+
+function App() {
+  return (
+    <div>
+    <Avatar img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" />
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(Createcard)}
+{/*
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />*/}
+    </div>
+  );
+}
+
+export default App;
